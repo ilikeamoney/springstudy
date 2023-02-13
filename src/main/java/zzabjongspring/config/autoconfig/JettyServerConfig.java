89171952ab24478814a691e0555aef7caaa7df6d
ilikeamoney <2721000@naver.com>
2023-02-13 20:09:09 +0900

    main

--- src/main/java/zzabjongspring/config/autoconfig/JettyServerConfig.java
+++ src/main/java/zzabjongspring/config/autoconfig/JettyServerConfig.java
@@ -1,27 +1,18 @@
 package zzabjongspring.config.autoconfig;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
+import zzabjongspring.config.ConditionalMyOnClass;
 import zzabjongspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
-@Conditional(JettyServerConfig.JettyCondition.class)
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyServerConfig {
     @Bean("jettyWebServerFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
-    }
-
-    static class JettyCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return false;
-        }
     }
 }
--- src/main/java/zzabjongspring/config/autoconfig/TomcatServerConfig.java
+++ src/main/java/zzabjongspring/config/autoconfig/TomcatServerConfig.java
@@ -1,26 +1,18 @@
 package zzabjongspring.config.autoconfig;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
+import zzabjongspring.config.ConditionalMyOnClass;
 import zzabjongspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
-@Conditional(TomcatServerConfig.TomcatCondition.class)
+@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatServerConfig {
     @Bean("tomcatWebServerFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
-    }
-
-    static class TomcatCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return true;
-        }
     }
 }
